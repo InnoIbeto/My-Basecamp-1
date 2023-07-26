ee74ed6c6ac4b052e0cc5ab69da9b100874ffcf6
InnoIbeto <inno.ibeto@gmail.com>
2023-07-26 16:37:52 +0100

    deleted sign up & sign in, generated a scafold and implemented CRUD funtionality of my basecamp using rails

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,4 @@ class HomeController < ApplicationController
   def index
   end
 
-  def signup
-  end
-
-  def signin
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :projects
 # get 'home/index'
   root 'home#index'
-  get 'home/signup'
-  get 'home/signin'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
